54e52879baa8582ce75df333f6b220b2755c24c1
oushiliji-dotcom <oushiliji@gmail.com>
2025-09-25 16:42:02 +0800

    Create preload.js

---
+++ preload.js
@@ -0,0 +1,7 @@
+const { contextBridge, ipcRenderer } = require('electron');
+
+contextBridge.exposeInMainWorld('electronAPI', {
+  getConfig: () => ipcRenderer.invoke('get-config'),
+  saveConfig: (config) => ipcRenderer.invoke('save-config', config),
+  runPhotoshop: (config) => ipcRenderer.invoke('run-photoshop', config)
+});
