e53b8d65fa6948c189ef2e22eefbd120a69ef948
oushiliji-dotcom <oushiliji@gmail.com>
2025-09-26 16:38:42 +0800

    Update preload.js

--- preload.js
+++ preload.js
@@ -1,7 +1,19 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('electronAPI', {
+  // 配置相关
   getConfig: () => ipcRenderer.invoke('get-config'),
   saveConfig: (config) => ipcRenderer.invoke('save-config', config),
-  runPhotoshop: (config) => ipcRenderer.invoke('run-photoshop', config)
+  
+  // 文件选择
+  selectDirectory: () => ipcRenderer.invoke('select-directory'),
+  selectFile: (filters) => ipcRenderer.invoke('select-file', filters),
+  
+  // Photoshop运行
+  runPhotoshop: (config) => ipcRenderer.invoke('run-photoshop', config),
+  
+  // 日志监听
+  onLogMessage: (callback) => {
+    ipcRenderer.on('log-message', (event, message) => callback(message));
+  }
 });
